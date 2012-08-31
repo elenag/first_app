ae3d97dbea9000d7ef02b9b94a93ecf1d634518e
Elena <elena@worldreader.org>
2012-08-31 03:48:19 +0300

    Remove duplicated column inaccess level db code

--- db/migrate/20120704211356_add_access_level.rb
+++ db/migrate/20120704211356_add_access_level.rb
@@ -1,9 +1,9 @@
 class AddAccessLevel < ActiveRecord::Migration
-  def up
-  	add_column("admin_users", "can_edit_origins", "boolean")
-  end
+  # def up
+  # 	add_column("admin_users", "can_edit_origins", "boolean")
+  # end
 
-  def down
-  	remove_column("admin_users", "can_edit_origins")
-  end
+  # def down
+  # 	remove_column("admin_users", "can_edit_origins")
+  # end
 end
