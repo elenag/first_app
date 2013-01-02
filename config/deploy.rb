aab9ad7fe70ccc5ba1487507b99c162b699ab17d
Elena <elena@worldreader.org>
2013-01-02 15:46:34 +0100

    Added deploy:migrate to the config/deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ role :db, "worldreader"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
-
+after "deploy", "deploy:migrate"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
