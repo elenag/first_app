c9c829b27000ae3221107826c2141dae9516c7d7
Elena <elena@worldreader.org>
2013-02-08 00:20:45 +0100

    Fix an error in categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name, :books_attributes, :id
+  attr_accessible :name, :books_attributes, :subcategories_attributes, :id
 
   has_many :books
   has_many :subcategories
+  accepts_nested_attributes_for :subcategories, :allow_destroy => true
 end
